a646b89a24e2ae2be7042d6059b37d46ff789ece
frankpezo <73926318+frankpezo@users.noreply.github.com>
2024-04-11 12:56:09 -0500

    Agregando contenido y map a las páginas

--- views/principal/contactos/index.php
+++ views/principal/contactos/index.php
@@ -17,86 +17,100 @@
 					</ul>
 				</div>
 			</div>
-		</div>
-<section class="our-rooms-area-two our-rooms-area-four ptb-100">
+
+	<!-- Start Contact Area -->
+	<section class="main-contact-area contact-info-area contact-info-three pt-100 pb-70">
 			<div class="container">
 				<div class="section-title">
-					<span>Our Rooms</span>
-					<h2>Fascinating rooms & suites</h2>
+					<span>Contact Us</span>
+					<h2>Drop us a message for any query</h2>
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque quibusdam deleniti porro praesentium. Aliquam minus quisquam velit in at nam.</p>
 				</div>
-				<div class="tab industries-list-tab">
-					<div class="row">
-						<div class="col-lg-5">
-							<div class="tabs row">
-								<div class="col-lg-6 col-sm-6 single-tab">
-									<div class="single-rooms">
-										<i class="flaticon-online-booking"></i>
-										<span class="booking-title">Free cost</span>
-										<h3>No booking fee</h3>
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="contact-wrap contact-pages">
+							<div class="contact-form contact-form-mb">
+								<form id="contactForm">
+									<div class="row">
+										<div class="col-lg-6 col-sm-6">
+											<div class="form-group">
+												<input type="text" name="name" id="name" class="form-control" required data-error="Please enter your name" placeholder="Your Name">
+												<div class="help-block with-errors"></div>
+											</div>
+										</div>
+			
+										<div class="col-lg-6 col-sm-6">
+											<div class="form-group">
+												<input type="email" name="email" id="email" class="form-control" required data-error="Please enter your email" placeholder="Your Email">
+												<div class="help-block with-errors"></div>
+											</div>
+										</div>
+			
+										<div class="col-lg-6 col-sm-6">
+											<div class="form-group">
+												<input type="text" name="phone_number" id="phone_number" required data-error="Please enter your number" class="form-control" placeholder="Your Phone">
+												<div class="help-block with-errors"></div>
+											</div>
+										</div>
+			
+										<div class="col-lg-6 col-sm-6">
+											<div class="form-group">
+												<input type="text" name="msg_subject" id="msg_subject" class="form-control" required data-error="Please enter your subject" placeholder="Your Subject">
+												<div class="help-block with-errors"></div>
+											</div>
+										</div>
+			
+										<div class="col-lg-12 col-md-12">
+											<div class="form-group">
+												<textarea name="message" class="form-control textarea-hight" id="message" cols="30" rows="4" required data-error="Write your message" placeholder="Your Message"></textarea>
+												<div class="help-block with-errors"></div>
+											</div>
+										</div>
+			
+										<div class="col-lg-12 col-md-12">
+											<button type="submit" class="default-btn btn-two">
+												Send Message
+												<i class="flaticon-right"></i>
+											</button>
+											<div id="msgSubmit" class="h3 text-center hidden"></div>
+											<div class="clearfix"></div>
+										</div>
 									</div>
-								</div>
-								<div class="col-lg-6 col-sm-6 single-tab">
-									<div class="single-rooms">
-										<i class="flaticon-podium"></i>
-										<span class="booking-title">Free cost</span>
-										<h3>Best rate guarantee</h3>
-									</div>
-								</div>
-								<div class="col-lg-6 col-sm-6 single-tab">
-									<div class="single-rooms">
-										<i class="flaticon-airport"></i>
-										<span class="booking-title">Free cost</span>
-										<h3>Reservations 24/7</h3>
-									</div>
-								</div>
-								<div class="col-lg-6 col-sm-6 single-tab">
-									<div class="single-rooms">
-										<i class="flaticon-slow"></i>
-										<span class="booking-title">Free cost</span>
-										<h3>High-speed Wi-Fi</h3>
-									</div>
-								</div>
-								<div class="col-lg-6 col-sm-6 single-tab">
-									<div class="single-rooms">
-										<i class="flaticon-coffee-cup-1"></i>
-										<span class="booking-title">Free cost</span>
-										<h3>Free breakfast</h3>
-									</div>
-								</div>
-								<div class="col-lg-6 col-sm-6 single-tab">
-									<div class="single-rooms">
-										<i class="flaticon-user"></i>
-										<span class="booking-title">100% free</span>
-										<h3>One person free</h3>
-									</div>
-								</div>
+								</form>
 							</div>
 						</div>
-						<div class="col-lg-7">
-							<div class="tab_content">
-								<div class="tabs_item">
-									<div class="our-rooms-single-img room-bg-1">
-									</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="row">
+							<div class="col-lg-6 col-sm-6">
+								<div class="single-contact-info">
+									<i class="bx bx-envelope"></i>
+									<h3>Email Us:</h3>
+									<a href="mailto:hello@arduix.com">hello@ecorik.com</a>
+									<a href="mailto:info@arduix.com">info@ecorik.com</a>
 								</div>
-								<div class="tabs_item">
-									<div class="our-rooms-single-img room-bg-2">
-									</div>
+							</div>
+							<div class="col-lg-6 col-sm-6">
+								<div class="single-contact-info">
+									<i class="bx bx-phone-call"></i>
+									<h3>Call Us:</h3>
+									<a href="tel:+(123)1800-567-8990">Tel. + (123) 1800-567-8990</a>
+									<a href="tel:+(124)1523-567-9874">Tel. + (124) 1523-567-9874</a>
 								</div>
-								<div class="tabs_item">
-									<div class="our-rooms-single-img room-bg-3">
-									</div>
+							</div>
+							<div class="col-lg-6 col-sm-6">
+								<div class="single-contact-info">
+									<i class="bx bx-location-plus"></i>
+									<h3>London</h3>
+									<a href="#">205 Fida Walinton, Tongo Street Front The USA</a>
 								</div>
-								<div class="tabs_item">
-									<div class="our-rooms-single-img room-bg-4">
-									</div>
-								</div>
-								<div class="tabs_item">
-									<div class="our-rooms-single-img room-bg-5">
-									</div>
-								</div>
-								<div class="tabs_item">
-									<div class="our-rooms-single-img room-bg-6">
-									</div>
+							</div>
+							<div class="col-lg-6 col-sm-6">
+								<div class="single-contact-info">
+									<i class="bx bx-phone-call"></i>
+									<h3>Call Us:</h3>
+									<a href="tel:+(123)1800-567-8990">Tel. + (123) 1800-567-8990</a>
+									<a href="tel:+(124)1523-567-9874">Tel. + (124) 1523-567-9874</a>
 								</div>
 							</div>
 						</div>
@@ -104,6 +118,14 @@
 				</div>
 			</div>
 		</section>
+		<!-- End Contact Area -->
+
+		<!-- Start Map Area -->
+		<div class="map-area">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.2392348655953!2d-73.24868748533667!3d-3.7501096976170826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91a6f19a7c6c3d97%3A0x36e0c87f5d3e1068!2sAv.%20Grau%201042%2C%20Iquitos%2016018%2C%20Peru!5e0!3m2!1sen!2sus!4v1649528066947!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+		</div>
+		<!-- End Map Area -->			
 <?php
 //2. Traemos el footer
 include_once 'views/template/footer-principal.php'
--- views/principal/habitacion/index.php
+++ views/principal/habitacion/index.php
@@ -5,7 +5,7 @@
 	<div class="page-title-area">
 			<div class="container">
 				<div class="page-title-content">
-					<h2>Habitacioness</h2>
+					<h2>Habitaciones</h2>
 					<ul>
 						<li>
 							<a href="index.html">
@@ -21,8 +21,8 @@
 <section class="our-rooms-area-two our-rooms-area-four ptb-100">
 			<div class="container">
 				<div class="section-title">
-					<span>Our Rooms</span>
-					<h2>Fascinating rooms & suites</h2>
+					<span>Habitacioness</span>
+					<h2>Facinantes habitaciones & suites</h2>
 				</div>
 				<div class="tab industries-list-tab">
 					<div class="row">
@@ -105,6 +105,236 @@
 			</div>
 		</section>
 
+
+		<section class="our-rooms-area pb-100"> 
+			<div class="container">
+				<div class="section-title">
+					<span>Our Rooms</span>
+					<h2>Fascinating rooms & suites</h2>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 col-sm-6">
+						<div class="single-rooms-three-wrap">
+							<div class="single-rooms-three">
+								<img src="<?php  echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/5.jpg" alt="Image">
+								<div class="single-rooms-three-content">
+									<h3>Deluxe Room for Relax</h3>
+									<ul class="rating">
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+									</ul>
+									<span class="price">From $50.6/night</span>
+									<a href="book-table.html" class="default-btn">
+										Book Online
+										<i class="flaticon-right"></i>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<div class="single-rooms-three-wrap">
+							<div class="single-rooms-three">
+								<img src="<?php  echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/6.jpg" alt="Image">
+								<div class="single-rooms-three-content">
+									<h3>Double Room</h3>
+									<ul class="rating">
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+									</ul>
+									<span class="price">From $60.6/night</span>
+									<a href="book-table.html" class="default-btn">
+										Book Online
+										<i class="flaticon-right"></i>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<div class="single-rooms-three-wrap">
+							<div class="single-rooms-three">
+								<img src="<?php  echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/7.jpg" alt="Image">
+								<div class="single-rooms-three-content">
+									<h3>Trippe Bed Room</h3>
+									<ul class="rating">
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+									</ul>
+									<span class="price">From $50.6/night</span>
+									<a href="book-table.html" class="default-btn">
+										Book Online
+										<i class="flaticon-right"></i>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<div class="single-rooms-three-wrap">
+							<div class="single-rooms-three">
+								<img src="<?php  echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/8.jpg" alt="Image">
+								<div class="single-rooms-three-content">
+									<h3>Window Amenities Room</h3>
+									<ul class="rating">
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+									</ul>
+									<span class="price">From $50.6/night</span>
+									<a href="book-table.html" class="default-btn">
+										Book Online
+										<i class="flaticon-right"></i>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<div class="single-rooms-three-wrap">
+							<div class="single-rooms-three">
+								<img src="<?php  echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/9.jpg" alt="Image">
+								<div class="single-rooms-three-content">
+									<h3>The royal room</h3>
+									<ul class="rating">
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+									</ul>
+									<span class="price">From $50.6/night</span>
+									<a href="book-table.html" class="default-btn">
+										Book Online
+										<i class="flaticon-right"></i>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<div class="single-rooms-three-wrap">
+							<div class="single-rooms-three">
+								<img src="<?php  echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/10.jpg" alt="Image">
+								<div class="single-rooms-three-content">
+									<h3>Budget Room</h3>
+									<ul class="rating">
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+										<li>
+											<i class="bx bxs-star"></i>
+										</li>
+									</ul>
+									<span class="price">From $90.6/night</span>
+									<a href="book-table.html" class="default-btn">
+										Book Online
+										<i class="flaticon-right"></i>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-12">
+						<div class="page-navigation-area">
+							<nav aria-label="Page navigation example text-center">
+								<ul class="pagination">
+									<li class="page-item">
+										<a class="page-link page-links" href="#">
+											<i class='bx bx-chevrons-left'></i>
+										</a>
+									</li>
+									<li class="page-item active">
+										<a class="page-link" href="#">1</a>
+									</li>
+									<li class="page-item">
+										<a class="page-link" href="#">2</a>
+									</li>
+									<li class="page-item">
+										<a class="page-link" href="#">3</a>
+									</li>
+									<li class="page-item">
+										<a class="page-link" href="#">
+											<i class='bx bx-chevrons-right'></i>
+										</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section> 
 <?php
 //2. Traemos el footer
 include_once 'views/template/footer-principal.php'
